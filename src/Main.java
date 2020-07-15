ba2f8d10224f3f9787d20c3da239bfbda55bb434
zurum ogbonda <zurum@zurums-MacBook-Pro.local>
2020-07-15 13:59:00 +0100

    add another print line

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("#JustJavaIt...");
+        System.out.println("Has the best Git tutorial for intelliJ");
     }
 }
