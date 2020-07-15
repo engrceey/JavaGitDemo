9746544d8a3cbeaed7dda8b20030e3a46f649432
Zurum <50442301+engrceey@users.noreply.github.com>
2020-07-15 14:21:33 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("#JustJavaIt...");
+        System.out.println("#JustJavaIt... Just Java it");
         System.out.println("Has the best Git tutorial for intelliJ");
     }
 }
